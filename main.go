344042382c27525388659e756c9fafa567e52e80
y.urakawa <s08a2015@gmail.com>
2018-05-09 14:31:43 +0900

    Added template

--- main.go
+++ main.go
@@ -3,7 +3,32 @@ package main
 import (
 	"net/http"
 	"log"
+	"sync"
+	"html/template"
+	"path/filepath"
 )
+
+
+// templ represents a single templat
+type templateHandler struct {
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+// ServerHTTP is a http.HandleFunc that renders this template.
+func (t *templateHandler) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	err := t.templ.Execute(w, nil)
+	if err  != nil {
+		log.Fatal(err)
+	}
+}
+
+
+
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
