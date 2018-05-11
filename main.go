923c1c6127f38815770cf3ffcbbf129b7d18a2fb
y.urakawa <s08a2015@gmail.com>
2018-05-11 17:36:22 +0900

    Add port number specification possible

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -20,20 +21,23 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	err := t.templ.Execute(w, nil)
+	err := t.templ.Execute(w, r)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "Application Address")
+	flag.Parse()
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// Open chat room
 	go r.run()
 	// Start web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Start web Server. Port:", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
