58e93f284a884f041dee5d9d210bd0fb705efc65
y.urakawa <s08a2015@gmail.com>
2018-06-04 10:17:22 +0900

    Add avatar_url and sign out.

--- main.go
+++ main.go
@@ -59,6 +59,16 @@ func main() {
 	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
 	// Open chat room
 	go r.run()
 	// Start web server
