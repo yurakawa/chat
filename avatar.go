0c0b41d304ad8e0750ac0ebea3e9dec9840d4f77
y.urakawa <s08a2015@gmail.com>
2018-06-06 10:19:34 +0900

    Add function for get Avatar URL

---
+++ avatar.go
@@ -0,0 +1,32 @@
+package main
+
+import "errors"
+
+// ErrNoAvatarURL is the error that is returned when the
+// Avatar instance is unable to provide an avatar URL.
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
+
+// Avatar represents types capable of representing
+// user profile pictures.
+type Avatar interface {
+	// GetAvatarURL gets the avatar URL for the specified client,
+	// or returns an error if something goes wrong.
+	// ErrNoAvatarURL is returned if the object is unable to get
+	// a URL for the specified client.
+	GetAvatarURL(c *client) (string, error)
+}
+
+// AuthAvatar represents avatar data obtained at the time of authentication
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+// GetAvatarURL returns AuthAvatar's avatar image URL
+func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
